e233d2a76050a93c0394ceec6afb6afe6520a218
Butini <marcbiedmabutier@gmail.com>
2021-07-14 19:13:38 +0200

    New entities. #5 Add Student and File
    Closes #5

---
+++ WcfStudent.Domain.Entity/File.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WcfStudent.Domain.Entity
+{
+    public class File
+    {
+        string Path { get; set; }
+        string FileName { get; set; }
+        string Extension { get; set; }
+
+        public File()
+        {
+        }
+
+        public File(string path, string fileName, string extension)
+        {
+            Path = path;
+            FileName = fileName;
+            Extension = extension;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is File file &&
+                   Path == file.Path &&
+                   FileName == file.FileName &&
+                   Extension == file.Extension;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -2071389000;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Path);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(FileName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Extension);
+            return hashCode;
+        }
+    }
+}
